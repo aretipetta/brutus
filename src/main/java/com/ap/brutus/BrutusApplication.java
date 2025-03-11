1800801aaeb6f04c0a04d0360dc7e2d6e5b436e7
areti <aretpett@gmail.com>
2025-03-11 23:46:32 +0200

    init commit

---
+++ src/main/java/com/ap/brutus/BrutusApplication.java
@@ -0,0 +1,13 @@
+package com.ap.brutus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrutusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrutusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ap/brutus/BrutusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.brutus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrutusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
